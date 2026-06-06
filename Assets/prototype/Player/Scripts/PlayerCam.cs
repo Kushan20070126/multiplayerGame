5e87262c5f252f27128dc63fd97bd69f8a2f7427
Kushan20070126 <k14922160@gmail.com>
2026-06-06 19:23:32 +0530

    Add player scripts and multiplayer settings

---
+++ Assets/prototype/Player/Scripts/PlayerCam.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+
+ public Transform orientation;
+
+ public float sensX;
+ public float sensY;
+
+float xRotation;
+float yRotation;
+ private void Start()
+ {
+  Cursor.lockState = CursorLockMode.Locked;
+  Cursor.visible = false;
+ }
+
+ private void Update()
+ {
+  float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensX;
+  float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensY;
+    yRotation += mouseX;
+    xRotation -= mouseY;
+    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+    transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+}
+}
